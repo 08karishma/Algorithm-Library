bc029c7b3714f482e624b1869ef202447b479bc6
Karishma Aggarwal <08karishma@gmail.com>
2019-09-22 12:00:57 +0530

    Updated 2D segment tree correct logic
    Earlier one had error in update query, also didnt have build logic

--- 2DSegmentTree.cpp
+++ 2DSegmentTree.cpp
@@ -1,68 +1,123 @@
-#include <iostream>
-#include <climits>
-#include <cstring>
-#define N 1025
-using namespace std;
+//https://leetcode.com/problems/range-sum-query-2d-mutable/submissions/
 
-int tree[4*N][4*N];
-int n;
+class NumMatrix {
+public:
+    vector<vector<int>> tree;
+    int R, C;
+    vector<vector<int>> arr;
+    
+    void buildInnerTree(int innerNode, int colStart, int colEnd, int outerNode, int rowStart, int rowEnd){
+        if(colStart == colEnd){
+            if(rowStart == rowEnd){
+                tree[outerNode][innerNode] = arr[rowStart][colStart];
+            }
+            else{
+                tree[outerNode][innerNode] = tree[2*outerNode][innerNode] + tree[2*outerNode+1][innerNode];
+            }
+            return;
+        }
+        int colMid = (colStart + colEnd)/2;
+        buildInnerTree(2*innerNode, colStart, colMid, outerNode, rowStart, rowEnd);
+        buildInnerTree(2*innerNode+1, colMid+1, colEnd, outerNode, rowStart, rowEnd);
+        tree[outerNode][innerNode] = tree[outerNode][2*innerNode] + tree[outerNode][2*innerNode + 1];        
+    }
+    
+    void buildOuterTree(int outerNode, int rowStart, int rowEnd){
+        if(rowStart != rowEnd){
+            int rowMid = (rowStart + rowEnd)/2;
+            buildOuterTree(outerNode*2, rowStart, rowMid);
+            buildOuterTree(outerNode*2 + 1, rowMid+1, rowEnd);
+        }
+        buildInnerTree(1, 0, C-1, outerNode, rowStart, rowEnd);
+    }
+    
+    void updateInnerTree(int innerNode, int colStart, int colEnd, int outerNode, int rowStart, int rowEnd, 
+                         int colIndex, int value)
+    {
+        if(colStart>colIndex || colEnd<colIndex){
+            return;
+        }
+        if(colStart == colEnd){
+            if(rowStart == rowEnd){
+                tree[outerNode][innerNode] = value;
+                arr[rowStart][colStart] = value;
+            }
+            else{
+                tree[outerNode][innerNode] = tree[2*outerNode][innerNode] + tree[2*outerNode+1][innerNode];
+            }
+            return;
+        }
+        int mid = (colStart + colEnd)/2;
+        updateInnerTree(2*innerNode, colStart, mid, outerNode, rowStart, rowEnd, colIndex, value);
+        updateInnerTree(2*innerNode+1, mid+1, colEnd, outerNode, rowStart, rowEnd, colIndex, value);
+        tree[outerNode][innerNode] = tree[outerNode][2*innerNode] + tree[outerNode][2*innerNode+1];
+    }
+    
+    void updateOuterTree(int outerNode, int rowStart, int rowEnd, int rowIndex, int colIndex, int value){
+        if(rowStart>rowIndex || rowEnd<rowIndex){
+            return;
+        }       
+        if(rowStart != rowEnd){
+            int mid = (rowStart + rowEnd)/2;
+            updateOuterTree(2*outerNode, rowStart, mid, rowIndex, colIndex, value);
+            updateOuterTree(2*outerNode+1, mid+1, rowEnd, rowIndex, colIndex, value);
+        }
+        updateInnerTree(1, 0, C-1, outerNode, rowStart, rowEnd, colIndex, value);
+    }
+    
+    int queryInnerTree(int innerNode, int colStart, int colEnd, int outerNode, int colLeft, int colRight){
+        if(colStart>colRight || colEnd<colLeft){
+            return 0;
+        }
+        if(colStart>=colLeft && colEnd<=colRight){
+            return tree[outerNode][innerNode];
+        }
+       int colMid = (colStart + colEnd)/2;
+       return queryInnerTree(2*innerNode, colStart, colMid, outerNode, colLeft, colRight)
+               +
+               queryInnerTree(2*innerNode+1, colMid+1, colEnd, outerNode, colLeft, colRight);
+    }
+    
+    int queryOuterTree(int outerNode, int rowStart, int rowEnd, int rowLeft, int rowRight, 
+                       int colLeft, int colRight)
+    {
+        if(rowStart>rowRight || rowEnd<rowLeft){
+            return 0;
+        }
+        if(rowStart>=rowLeft && rowEnd<=rowRight){
+            return queryInnerTree(1, 0, C-1, outerNode, colLeft, colRight);
+        }
+        int rowMid = (rowStart + rowEnd)/2;
+        return queryOuterTree(2*outerNode, rowStart, rowMid, rowLeft, rowRight, colLeft, colRight)
+            +
+                queryOuterTree(2*outerNode+1, rowMid+1, rowEnd, rowLeft, rowRight, colLeft, colRight);
+    }
+    
+    NumMatrix(vector<vector<int>>& matrix) {
+        R = matrix.size();
+        if(R == 0) return;
+        C = matrix[0].size();
+        
+        tree.resize(4*R);
+        for(int i=0; i<4*R; i++){
+            tree[i].resize(4*C);
+        }
+        arr = matrix;
+        buildOuterTree(1, 0, R-1);
+    }
+    
+    void update(int row, int col, int val) {
+        updateOuterTree(1, 0, R-1, row, col, val);
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return queryOuterTree(1, 0, R-1, row1, row2, col1, col2);
+    }
+};
 
-void update(int node, int s, int e, int y, int val, int o){
-	if(s>y || e<y) return;
-	if(s==e){
-		tree[o][node]=val;
-		return;
-	}
-	int m=(s+e)/2;
-	update(2*node, s, m, y, val, o);
-	update(2*node+1, m+1, e, y, val, o);
-	tree[o][node] = tree[o][2*node]+tree[o][2*node+1];
-}
-
-void update1(int node, int s, int e, int x, int y, int val){
-	if(s>x || e<x) return;
-	if(s==e){
-		update(1, 0, n-1, y, val, node);
-		return;
-	}
-	int m=(s+e)/2;
-	update1(2*node, s, m, x, y, val);
-	update1(2*node+1, m+1, e, x, y, val);
-	update(1, 0, n-1, y, val, node);
-}
-
-int sum(int node, int s, int e, int yl, int yr, int o){ //o=outer tree index
-	if(s>yr || e<yl) return 0;
-	if(s>=yl && e<=yr) return tree[o][node];
-	int m=(s+e)/2;
-	return sum(2*node, s, m, yl, yr, o)+sum(2*node+1, m+1, e, yl, yr, o);
-}
-
-int sum1(int node, int s, int e, int xl, int xr, int yl, int yr){
-	if(s>xr || e<xl) return 0;
-	if(s>=xl && e<=xr) return sum(1, 0, n-1, yl, yr, node);
-	int m=(s+e)/2;
-	return sum1(2*node, s, m, xl, xr, yl, yr)+sum1(2*node+1, m+1, e, xl, xr, yl, yr);
-}
-
-int main() {
-	int t; scanf("%d", &t);
-	while(t--){
-		scanf("%d", &n);
-		memset(tree, 0, sizeof tree);
-		while(1){
-			string s; cin>>s;
-			if(s=="END") break;
-			if(s=="SET"){
-				int x,y,val; scanf("%d%d%d", &x, &y, &val);
-				update1(1, 0, n-1, x, y, val);
-			}
-			else if(s=="SUM"){
-				int x1, y1, x2, y2; scanf("%d%d%d%d", &x1, &y1, &x2, &y2);
-				printf("%d\n", sum1(1, 0, n-1, x1, x2, y1, y2));
-			}
-		}
-		printf("\n");
-	}
-	return 0;
-}
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * obj->update(row,col,val);
+ * int param_2 = obj->sumRegion(row1,col1,row2,col2);
+ */
